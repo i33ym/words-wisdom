0f63f987c78ab73284ebc44c5e0eb01aa6c351a3
i33ym <isroil.muhitdinov14@gmail.com>
2024-12-07 12:22:38 +0500

    basic template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,7 +1,32 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
+
+type data struct {
+	ID  int
+	Day string
+}
 
 func (app *application) home(response http.ResponseWriter, request *http.Request) {
-	response.Write([]byte("Welcome Home...\n"))
+	ts, err := template.ParseFiles("./ui/html/home.html")
+	if err != nil {
+		app.logger.Printf("failed to parse the file: %s", err)
+		response.Write([]byte("something terrible happened. sorry!\n"))
+
+		return
+	}
+
+	data := &data{
+		ID:  123,
+		Day: "Friday",
+	}
+	if err := ts.Execute(response, data); err != nil {
+		app.logger.Printf("failed to execute the template: %s", err)
+		response.Write([]byte("something terrible happened. sorry!\n"))
+
+		return
+	}
 }
