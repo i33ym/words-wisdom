198bbeb2cd088f2a75238a7d05dc59f7bf7db7ec
i33ym <isroil.muhitdinov14@gmail.com>
2024-12-07 17:58:10 +0500

    template composition

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,13 +5,12 @@ import (
 	"text/template"
 )
 
-type data struct {
-	ID  int	
-	Day string
-}
-
 func (app *application) home(response http.ResponseWriter, request *http.Request) {
-	ts, err := template.ParseFiles("./ui/html/home.html")
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/home.html",
+	}
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		app.logger.Printf("failed to parse the file: %s", err)
 		response.Write([]byte("something terrible happened. sorry!\n"))
@@ -19,35 +18,31 @@ func (app *application) home(response http.ResponseWriter, request *http.Request
 		return
 	}
 
-	data := &data{
-		ID:  123,
-		Day: "Friday",
-	}
-	if err := ts.Execute(response, data); err != nil {
+	if err := ts.ExecuteTemplate(response, "base", nil); err != nil {
 		app.logger.Printf("failed to execute the template: %s", err)
 		response.Write([]byte("something terrible happened. sorry!\n"))
 
 		return
 	}
+}
 
-	// content := `
-	// <html>
-	// 	<head>
-	// 		<title>%s</title>
-	// 	</head>
-	// 	<body>
-	// 		<h4>hello</h4>
-	// 		<p>id: %d</p>
-	// 		<p>enjoy your %s</p>
-	// 	</body>
-	// </html>
-	// `
+func (app *application) about(response http.ResponseWriter, request *http.Request) {
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/about.html",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.logger.Printf("failed to parse the file: %s", err)
+		response.Write([]byte("something terrible happened. sorry!\n"))
 
-	// dt := &data{
-	// 	id: 123,
-	// 	day: "saturday",
-	// }
+		return
+	}
 
-	// content = fmt.Sprintf(content, "perfect title", dt.id, dt.day)
-	// response.Write([]byte(content))
+	if err := ts.ExecuteTemplate(response, "base", nil); err != nil {
+		app.logger.Printf("failed to execute the template: %s", err)
+		response.Write([]byte("something terrible happened. sorry!\n"))
+
+		return
+	}
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,5 +5,6 @@ import "net/http"
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/about", app.about)
 	return mux
 }
